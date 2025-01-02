cbb46e989df13dcecdd55c5ca4290dc9e9dedb9a
ijapesigan <ijapesigan@gmail.com>
2025-01-02 06:24:19 +0000

    Automated build.

--- scripts/ubuntu_cran_pkg.sh
+++ scripts/ubuntu_cran_pkg.sh
@@ -5,22 +5,22 @@
 set -e
 
 install2.r --error --skipinstalled -n -1 \
-    cffr                                 \
-    covr                                 \
-    devtools                             \ 
-    distro                               \
-    knitr                                \
-    lintr                                \
-    pkgdown                              \
-    quarto                               \
-    Rcpp                                 \
-    remotes                              \
-    RefManageR                           \
-    rmarkdown                            \
-    roxygen2                             \
-    rprojroot                            \
-    stats                                \
-    styler                               \
-    tinytex                              \
-    utils                                \
+    cffr       \
+    covr       \
+    devtools   \
+    distro     \
+    knitr      \
+    lintr      \
+    pkgdown    \
+    quarto     \
+    Rcpp       \
+    remotes    \
+    RefManageR \
+    rmarkdown  \
+    roxygen2   \
+    rprojroot  \
+    stats      \
+    styler     \
+    tinytex    \
+    utils      \
     testthat
